bc97cf9da5985a012aa8e94442525eb2ee345a87
risCodeSharp <rishavsingh2006@gmail.com>
2026-04-13 21:05:14 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'NotesInventory',
+  base: '/notes_taking/',
   plugins: [
     vue(),
     vueDevTools(),
