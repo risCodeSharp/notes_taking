bd07c8ec666711f36072b9843fd6d41b35028048
Candyrizz <251220677+risCodeSharp@users.noreply.github.com>
2026-04-13 21:30:02 +0530

    s

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,13 @@
 import RegisterView from '@/views/RegisterView.vue'
 import EditorView from '@/views/EditorView.vue'
 import LoginView from '@/views/LoginView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+// 1. Change createWebHistory to createWebHashHistory
+import { createRouter, createWebHashHistory } from 'vue-router' 
 import AccountSettingsView from '@/views/accountSettingsView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // 2. Update this line to use the hash history
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
@@ -31,4 +33,3 @@ const router = createRouter({
 })
 
 export default router
-
