c2c1300538de1d86c1e8fc3fa4fd7d72bf9315b6
Candyrizz <251220677+risCodeSharp@users.noreply.github.com>
2026-04-13 21:15:30 +0530

    fix it for redirecting pages

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,7 +5,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AccountSettingsView from '@/views/accountSettingsView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/notes_taking/'),
   routes: [
     {
       path: "/",
